58d02d6dcc6e6ceaea502dee02b321b4750b23ce
philipwanjiku <muhorophilip7@gmail.com>
2023-10-30 11:26:02 +0300

    Completed Assignment

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// document.addEventListener('DOMContentLoaded',getUsers);
+
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+    const searchbx = document.querySelector('#search');
+    const searchbtn = document.querySelector('#submit');
+    const userApi = 'https://api.github.com/search/users?q=';
+    const repoApi = 'https://api.github.com/users/octocat/repos';
+
+
+
+    async function checkUser(user){
+    const response1= await fetch(userApi + user)
+    const data = await response1.json()
+    const userlist=document.querySelector(`#user-list`)
+    userlist.innerHTML=``
+    user.forEach((item) => {
+        const userName = document.createElement(`li`)
+        userName.textContent= item.login
+        userlist.appendChild(userName)
+
+    });
+}
+
+    async function checkRepo(){
+        const response2 = await fetch(repoApi)
+        const data = await response2.json()
+        const repolist = document.querySelector(`#repos-list`)
+        repolist.innerHTML=``
+        data.forEach((item)=>{
+            const repoName = document.createElement(`li`)
+            repoName.textContent=item.name;
+            repolist.appendChild(repoName)
+        })
+}
+searchbtn.addEventListener(`click`,(e)=>{
+    e.preventDefault()
+    let userInsert = searchbx.value
+    checkRepo(userInsert)
+    checkUser(userInsert)
+})
+})
